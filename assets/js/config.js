f60e87ba10deed2c767ad15b9de6ceb2c69dca3f
任天赋 <rentianfu@xiaomi.com>
2019-10-23 19:19:59 +0800

    feat:微信登录

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,12 +1,23 @@
-// var HOST = 'http://localhost:9999' // todo
+// var HOST = 'http://www.renwuming.cn/jmz' // TODO
+// var ROOT_HOST = 'http://www.renwuming.cn/auth' // TODO
+// var CALLBACK = 'http://www.renwuming.cn/jmz-fe' // TODO
 var HOST = 'https://www.renwuming.cn/jmz'
-
+var AUTH_HOST = 'https://www.renwuming.cn/auth'
+var CALLBACK = 'https://www.renwuming.cn/jmz-fe'
 
 function getQuery(key) {
-    var result = location.search.substr(1).match(new RegExp('(?:^|&)' + key + '=(.+?)(?:$|&)'));
-    return result ? result[1] : result;
+  var result = location.search
+    .substr(1)
+    .match(new RegExp('(?:^|&)' + key + '=(.+?)(?:$|&)'))
+  return result ? result[1] : result
 }
 
 function gotoLogin() {
-    window.location = `./login?callback=${encodeURIComponent(window.location)}`
+  $.ajax({
+    url: AUTH_HOST + '/user/login/url?redirect=' + CALLBACK,
+    type: 'GET',
+    success: function(url) {
+      window.location = url
+    }
+  })
 }
